1ae87d5a63a66d9474ee9ca45e9c3fa7d289a4de
admin_pk\Admin <Cojucovschi@gmail.com>
2018-01-30 11:56:39 +0200

    Json test forst commit

---
+++ XamarinJsonSiteTest/XamarinJsonSiteTest/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace XamarinJsonSiteTest
+{
+    [Activity(Label = "XamarinJsonSiteTest", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        public TextView login;
+        public TextView loginText;
+        public TextView passwordText;
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            login = FindViewById<TextView>(Resource.Id.login);
+            loginText = FindViewById<TextView>(Resource.Id.email);
+            passwordText = FindViewById<TextView>(Resource.Id.password);
+
+
+        }
+    }
+}
+
